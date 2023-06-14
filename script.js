4b29cbde806f200581f24d02a9d633523f81dd20
Aung Pyay Thar <132377611+aungpyaythar1998@users.noreply.github.com>
2023-06-14 18:20:41 +0630

    Finish the game functionality with barebone UI

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function winIfEqual(selection, winCondition) {
     let result;
     result = (selection === winCondition) ? "win" : "lose";
@@ -21,10 +22,39 @@ function playRound(playerSelection, computerSelection) {
         case "scissor":
             roundResult = winIfEqual(computerSelection, "paper");
             break;
-        default:
-            return "invalid";
     }
     return roundResult;
+}
+
+function resetGame() {
+    userScore = 0;
+    computerScore = 0;
+    updateUserScoreUI(userScore);
+    updateComputerScoreUI(computerScore);
+}
+
+function announceWinner() {
+    if (userScore == roundsToWin && computerScore == roundsToWin) {
+        resultDisplay.textContent = "You drew ";
+    } else if (userScore == roundsToWin) {
+        resultDisplay.textContent = "You won ";
+    } else if (computerScore == roundsToWin) {
+        resultDisplay.textContent = "You lost ";
+    }
+    resultDisplay.textContent += `${userScore} vs ${computerScore} against computer!`
+}
+
+function updateUserScoreUI(score) {
+    userScoreUI.textContent = score;
+}
+
+function updateComputerScoreUI(score) {
+    computerScoreUI.textContent = score;
+}
+
+function announceComputerChoice(choice) {
+    const computerChoiceUI = document.querySelector("#computer-choice");
+    computerChoiceUI.textContent = choice;
 }
 
 function getComputerChoice() {
@@ -32,6 +62,45 @@ function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3);
     return options[randomChoice];
 }
+
+
+function playGame(e) {
+    const userChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+
+    announceComputerChoice(computerChoice);
+    const roundResult = playRound(userChoice, computerChoice);
+    if (roundResult == "win") {
+        userScore += 1;
+        updateUserScoreUI(userScore);
+    } else if (roundResult == "lose") {
+        computerScore += 1;
+        updateComputerScoreUI(computerScore);
+    } else if (roundResult == "draw") {
+        userScore += 1;
+        updateUserScoreUI(userScore);
+        computerScore += 1;
+        updateComputerScoreUI(computerScore);
+    }
+
+    if(userScore == roundsToWin || computerScore == roundsToWin) {
+        announceWinner();
+        resetGame();
+    }
+}
+
+
+let userScore = 0;
+let computerScore = 0;
+const userScoreUI = document.querySelector("#user-score");
+const computerScoreUI = document.querySelector("#computer-score");
+const resultDisplay = document.querySelector("#result");
+const roundsToWin = 5;
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(eachButton => {
+    eachButton.addEventListener("click", playGame);
+});
 
 
 
